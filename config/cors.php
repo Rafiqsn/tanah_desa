3a101fb9719e6d180f515f1c9e6793179611ab4f
Rafiqsn <rafiqsusetya17@gmail.com>
2025-11-29 20:13:37 +0700

    tes akhir

---
+++ config/cors.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'http://3.226.122.125:3000',
+        'http://localhost:3000',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
